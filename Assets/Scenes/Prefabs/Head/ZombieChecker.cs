cdbe406526c24a6295dae278cd8f1736deffad14
Alexander Pu <alexander.f.pu@gmail.com>
2021-01-24 12:07:23 -0800

    Change reticle color on interaction with zombie

--- Assets/Scenes/Prefabs/Head/ZombieChecker.cs
+++ Assets/Scenes/Prefabs/Head/ZombieChecker.cs
@@ -10,6 +10,8 @@ public class ZombieChecker : MonoBehaviour
     float secondsOnZombie;
     [SerializeField]
     Image radialProgressBar;
+    [SerializeField]
+    Image reticle;
 
     private void Start() {
         layerMask = 1 << 8;
@@ -26,12 +28,15 @@ public class ZombieChecker : MonoBehaviour
 
             if (!mask.isMasked()) {
                 handleUnmaskedZombie(hitZombie, mask);
+                reticle.color = Color.green;
             } else {
                 radialProgressBar.fillAmount = 0;
+                reticle.color = Color.white;
             }
         } else {
             radialProgressBar.fillAmount = 0;
             currentZombie = null;
+            reticle.color = Color.white;
         }
     }
 
