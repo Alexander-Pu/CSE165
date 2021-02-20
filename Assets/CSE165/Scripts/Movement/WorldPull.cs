e9ec0986db4741194322fde35c558421ad76b202
Alex Pu <alexander.f.pu@gmail.com>
2021-02-19 21:23:28 -0800

    Project 3 - Removed files from previous project, implemented furniture spawning, hand grabbing movement

---
+++ Assets/CSE165/Scripts/Movement/WorldPull.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPull : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pulledTransform;
+    [SerializeField]
+    private Transform leftAnchor;
+    [SerializeField]
+    private Transform rightAnchor;
+    [SerializeField]
+    private float translateMultiplier = 5;
+
+    private Vector3 prevAnchorPosition;
+    private Transform referenceAnchor = null;
+    private float prevLeftVal = 0;
+    private float prevRightVal = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftVal = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        float rightVal = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+
+        // Nothing to do if neither grips are active
+        // OR
+        // Clear values if either of the grips are released
+        // This makes it stutter for a frame, but it's not really noticable
+        if ((0 == leftVal && 0 == rightVal) || 
+            (IsNewRelease(prevLeftVal, leftVal) || IsNewRelease(prevRightVal, rightVal)))
+        {
+            UpdateValues(0, 0);
+            return;
+        }
+
+        // Determine if a new press has been encountered
+        bool rightNew = IsNewPress(prevRightVal, rightVal);
+        bool leftNew = IsNewPress(prevLeftVal, leftVal);
+
+        // Right hand is the dominant hand, let right hand resolve ties
+        if (rightNew)
+        {
+            referenceAnchor = rightAnchor;
+            UpdateValues(leftVal, rightVal);
+            return;
+        } else if (leftNew)
+        {
+            referenceAnchor = leftAnchor;
+            UpdateValues(leftVal, rightVal);
+            return;
+        }
+
+        // At least one hand is active, and neither are new
+        Vector3 anchorCurrentPosition = referenceAnchor.position;
+        Vector3 translateVector = translateMultiplier * (prevAnchorPosition - anchorCurrentPosition);
+
+        pulledTransform.Translate(translateVector);
+
+        UpdateValues(leftVal, rightVal);
+    }
+
+    private void UpdateValues(float leftVal, float rightVal)
+    {
+        prevLeftVal = leftVal;
+        prevRightVal = rightVal;
+        if (null != referenceAnchor)
+        {
+            prevAnchorPosition = referenceAnchor.position;
+        }
+    }
+
+    private bool IsNewPress(float prevVal, float newVal)
+    {
+        return 0 == prevVal && 0 != newVal;
+    }
+
+    private bool IsNewRelease(float prevVal, float newVal)
+    {
+        return 0 != prevVal && 0 == newVal;
+    }
+}
---
+++ Assets/CSE165/Scripts/Spawner/FurnitureSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject primaryFurniture;
+    [SerializeField]
+    private GameObject secondaryFurniture;
+    [SerializeField]
+    private Transform player;
+
+    private GameObject heldGameObject = null;
+
+    void Update()
+    {
+        // Spawn
+        bool aPress = OVRInput.GetDown(OVRInput.Button.Two);
+        bool bPress = OVRInput.GetDown(OVRInput.Button.One);
+
+        // aPress is dominant in case of tie
+        if (aPress)
+        {
+            SpawnFurniture(primaryFurniture);
+            return;
+        } else if (bPress)
+        {
+            SpawnFurniture(secondaryFurniture);
+            return;
+        }
+
+        // Release if either button is released. Simplifies logic
+        bool aRelease = OVRInput.GetUp(OVRInput.Button.Two);
+        bool bRelease = OVRInput.GetUp(OVRInput.Button.One);
+        if (aRelease || bRelease)
+        {
+            ReleaseFurniture();
+        }
+    }
+
+    private void SpawnFurniture(GameObject prefab)
+    {
+        if (null == heldGameObject)
+        {
+            heldGameObject = Instantiate(prefab);
+            heldGameObject.transform.SetParent(transform);
+            heldGameObject.transform.position = transform.position;
+            heldGameObject.transform.LookAt(player);
+
+            Rigidbody rigidBody = heldGameObject.GetComponent<Rigidbody>();
+            if (null != rigidBody)
+            {
+                rigidBody.isKinematic = true;
+            }
+        }
+    }
+
+    private void ReleaseFurniture()
+    {
+        if (null != heldGameObject)
+        {
+            Rigidbody rigidBody = heldGameObject.GetComponent<Rigidbody>();
+            if (null != rigidBody)
+            {
+                rigidBody.isKinematic = false;
+            }
+            heldGameObject.transform.SetParent(null);
+            heldGameObject = null;
+        }
+    }
+}
